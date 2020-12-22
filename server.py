9860d320fb8925e399310b36f465ec1046b304a5
DrinoSan <s.becirbegovic@gmail.com>
2020-12-22 22:12:10 +0000

    server

--- server.py
+++ server.py
@@ -3,10 +3,13 @@ import socket
 IP = "95.179.188.56"
 PORT = 5001
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((IP, PORT))
 server.listen()
 
 while True:
     client, address = server.accept()
     print(address)
-    client.send("TEXT")
+    client.send("TEXT".encode("utf-8"))
+    m = client.recv(1024).decode("utf-8")
+    print(m)
