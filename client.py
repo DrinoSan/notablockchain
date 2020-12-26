4976fd4a952c821554921a654a85bf9aeda6bd93
Sandrino Becirbegovic <s.becirbegovic@gmail.com>
2020-12-26 18:12:29 +0100

    Client

--- client.py
+++ client.py
@@ -1,6 +1,27 @@
+import hashlib
+import json
+from time import time
+from urllib.parse import urlparse
+from uuid import uuid4
+
+
+import threading
 import socket
-from server import IP, PORT
+import select
+
+import requests
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
+if __name__ == "__main__":
+
+    IP = "95.179.188.56"
+    PORT = 5002
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, PORT))
+
+    m = client.recv(1024).decode()
+    print(m)
+    test_msg = "TEST this is coming from the client"
+    # s = input("PLEASE ENTER TEYXT").encode()
+    client.send(test_msg.encode())
